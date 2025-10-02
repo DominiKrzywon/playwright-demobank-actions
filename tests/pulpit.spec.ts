53c5fc33e8e797a6df3053360b9d485c489b2a49
Dominik <d_krzywon@o2.pl>
2025-10-02 20:26:09 +0200

    Update pulpit.spec.ts

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -31,7 +31,7 @@ test.describe('Pulpit tests', () => {
       const receiverId = '2';
       const transferAmount = '150';
       const transferTitle = 'pizza';
-      const expectedTransferReceiver = 'Chuck Demobankowy XYZ';
+      const expectedTransferReceiver = 'Chuck Demobankowy';
 
       // Act
       await pulpitPage.executeQuickPayment(
